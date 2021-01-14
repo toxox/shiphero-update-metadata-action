ba0f94decc169adb4e330d64a6832ad50d7d658b
toxox <anton.trublin@gmail.com>
2021-01-14 14:28:17 -0800

    add ability to set isMandatory flag and release channel

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import * as crypto from 'crypto'
 import * as path from 'path'
 import glob from 'fast-glob'
 
+const CHANNEL_REGEX = /-(alpha|beta)\d*$/g
+
 const generateUpdateMetadata = async (): Promise<void> => {
   const isMac = core.getInput('os').startsWith('macos')
   const version = core.getInput('version')
+  const isMandatory = core.getInput('version')
   const fileExt = isMac ? 'zip' : 'exe'
 
   const [updatePath] = await glob(`./dist/*.${fileExt}`)
@@ -19,19 +22,21 @@ const generateUpdateMetadata = async (): Promise<void> => {
     version,
     filePath: updateFileName,
     releaseDate: new Date().toISOString(),
-    isMandatory: false,
+    isMandatory,
     md5
   }
 
+  const channelFromVersion = version.match(CHANNEL_REGEX)
+  const channel = channelFromVersion
+    ? channelFromVersion[0].replace(/\W/, '')
+    : 'latest'
+
   await fs.mkdir('./release', {recursive: true})
   await fs.writeFile(
-    `./release/latest${isMac ? '-mac' : ''}.json`,
+    `./release/${channel}${isMac ? '-mac' : ''}.json`,
     JSON.stringify(metadata)
   )
   await fs.rename(updatePath, `./release/${updateFileName}`)
-
-  const a = await fs.readFile(`./release/${updateFileName}`)
-  console.log(a)
 }
 
 try {
